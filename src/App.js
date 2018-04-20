f2ddcf64e513285b543118ac4830422d2fe8dbfe
Steven <stevenwright1012@gmail.com>
2018-04-20 11:12:08 -0600

    routes set up, need to fix nav bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav/Nav';
-import Auth from './components/Auth/Auth';
-import Dashboard from './components/Dashboard/Dashboard';
-import Form from './components/Form/Form';
-import Post from './components/Post/Post';
+// import Auth from './components/Auth/Auth';
+// import Dashboard from './components/Dashboard/Dashboard';
+// import Form from './components/Form/Form';
+// import Post from './components/Post/Post';
+import routes from './routes';
+import {withRouter} from 'react-router-dom';
 
 class App extends Component {
   render() {
+    console.log(this.props);
+    
     return (
       <div className="App">
+        {/* {
+          this.props.location.pathname === '/'
+          ?
+          null
+          :
+          <Nav />
+        } */}
         <Nav />
-        <Auth />
-        <Dashboard />
-        <Form />
-        <Post />
+        {routes}
       </div>
     );
   }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Nav(){
     return(
         <div>
             <h1>Nav</h1>
+            <Link to='/dashboard'>
+                <button>Home</button>
+            </Link>
+            <Link to='/new'>
+                <button>New Post</button>
+            </Link>
+            <Link to='/'>
+                <button>Logout</button>
+            </Link>
         </div>
     )
 }
